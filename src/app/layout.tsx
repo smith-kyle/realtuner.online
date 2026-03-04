8c493063e5905f2e9d06c5bac46faa041bb3f0b4
Kyle Smith <kyle@gitnotebooks.com>
2026-03-03 17:12:06 -0800

    Added favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,9 @@ const geistMono = Geist_Mono({
 })
 
 export const metadata: Metadata = {
-  title: 'RealTuner Online - Live Instrument Tuning Queue',
+  title: 'RealTuner.online - Tune on a real Boss TU-3',
   description:
-    'Join the queue and tune your instrument live with others. Real-time audio streaming for musicians.',
+    'Real guitarists use real tuner.',
 }
 
 export const viewport: Viewport = {
@@ -32,6 +32,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="RealTuner" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
+
       <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
     </html>
   )
